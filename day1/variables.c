64cd86c7effd0b0366f6f093eec14fdc79b46728
SalimuKabogere <salimuloma@gmail.com>
2026-07-08 12:39:17 +0300

    add: print my name by declaring it as a variable first, great work

---
+++ day1/variables.c
@@ -0,0 +1,9 @@
+// i want to use variables to print my name
+// not so much has been learnt but i want to see the best i can get here
+
+#include <stdio.h>
+int main (){
+    char name[] = "Salimu Kabogere";
+    printf ("My name is %s\n", name);
+    return 0;
+}
