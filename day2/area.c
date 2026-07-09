0e0359f0f8f1fb99bd617fa7fac354db6ab3881d
SalimuKabogere <salimuloma@gmail.com>
2026-07-09 10:31:38 +0300

    add: declare the variables in the same line separated by commas since they take the same data type

--- day2/area.c
+++ day2/area.c
@@ -7,5 +7,9 @@ int main (){
     int width = 10;
     int area = length * width;
     printf ("The area of the rectangle is %d\n", area);
+
+    // or you can declare the variables in a single line since they all have the same type
+    int length2 = 5, width2 = 10, area2 = length2 * width2;
+    printf ("The area of the rectangle is %d\n", area2);
     return 0;
 }
