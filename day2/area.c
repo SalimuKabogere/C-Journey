41d15e66bbabbb8c9699f1a5f78af042ab4f481a
SalimuKabogere <salimuloma@gmail.com>
2026-07-09 10:29:01 +0300

    add: calculate the area of a rectangle using its formular and declaring variables

---
+++ day2/area.c
@@ -0,0 +1,11 @@
+// This program calculates the area of a rectangle
+/*Area of a rectangle = length * width*/
+
+#include <stdio.h>
+int main (){
+    int length = 5;
+    int width = 10;
+    int area = length * width;
+    printf ("The area of the rectangle is %d\n", area);
+    return 0;
+}
