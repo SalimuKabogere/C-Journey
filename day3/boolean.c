2c3f8defc283ceca6979f0c4d7ffd1dec5cb3eb8
SalimuKabogere <salimuloma@gmail.com>
2026-07-13 17:16:43 +0300

    add: booleans

---
+++ day3/boolean.c
@@ -0,0 +1,14 @@
+// import the standard boolean library
+#include <stdbool.h>
+#include <stdio.h>
+int main (){
+    // declare a boolean variable
+    bool isRaining = true;
+    bool isSunny = false;
+
+    // print the value of the boolean variables
+    printf("Is it raining? %d\n", isRaining); // prints 1, which means true
+    printf("Is it sunny? %d\n", isSunny); // prints 0, which means false
+
+    return 0;
+}
