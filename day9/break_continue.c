9b820174f9ac9cc69c45e9255468185a2c5ff9f5
SalimuKabogere <salimuloma@gmail.com>
2026-07-22 11:13:55 +0300

    Add: Show how continue and break can be combined in a loop

--- day9/break_continue.c
+++ day9/break_continue.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 int main (){
     for (int i=1;i<=10;i++){
-        // if(i==4){
-        //     break; // execution jumps out of the loop when i is 4
-        // }
-
-        // for continue, it skips the execution of the loop when i is 4 and continues with the next iteration
-        if(i==4){
-            continue; // skips the rest of the loop when i is 4
+        if(i==2){
+            continue; //skips 2 during execution of the loop
         }
-        printf("%d ", i); //prints 1 2 3 5 6 7 8 9 10
+
+        if(i==4){
+            break; //stops when the loop gets to 4
+        }
+        printf("%d ", i); //prints 1 and 3
     }
     return 0;
 }
