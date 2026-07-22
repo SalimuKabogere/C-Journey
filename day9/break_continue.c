9f6e7d678945b1caa122ba30c4261e0d5b928664
SalimuKabogere <salimuloma@gmail.com>
2026-07-22 11:11:13 +0300

    Add: Show how continue works in a for loop by skipping the execution

--- day9/break_continue.c
+++ day9/break_continue.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 int main (){
     for (int i=1;i<=10;i++){
+        // if(i==4){
+        //     break; // execution jumps out of the loop when i is 4
+        // }
+
+        // for continue, it skips the execution of the loop when i is 4 and continues with the next iteration
         if(i==4){
-            break; // execution jumps out of the loop when i is 4
+            continue; // skips the rest of the loop when i is 4
         }
-        printf("%d ", i); //prints 1 2 3
+        printf("%d ", i); //prints 1 2 3 5 6 7 8 9 10
     }
     return 0;
 }
