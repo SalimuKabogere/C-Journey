b4cbc9bb261e689eb7ff4e10d096aa5bab465fb7
SalimuKabogere <salimuloma@gmail.com>
2026-07-23 16:46:39 +0300

    add: function examples, like one to calculate the sum of two numbers

--- day10/functions.c
+++ day10/functions.c
@@ -5,8 +5,24 @@ void greet(){
     printf("Hello, World!\n");  
 }
 
+// function to calculate the sum of two integers
+int sum(int a, int b){
+    return a+b;
+}
+
+void calculateSum(){
+    int a,b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    int result = sum(a, b);
+    printf("The sum of %d and %d is: %d\n", a, b, result);
+}
+
 int main(){
     // function call
     greet();
+    int result = sum(5, 10);
+    printf("The sum of 5 and 10 is: %d\n", result);
+    calculateSum();
     return 0;
 }
