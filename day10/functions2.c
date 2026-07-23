19458f4c4c3270cc7ea37f0fad4fb65c89685a2e
SalimuKabogere <salimuloma@gmail.com>
2026-07-23 17:35:11 +0300

    add: a function that converts Celsius to Fahrenheit

--- day10/functions2.c
+++ day10/functions2.c
@@ -7,9 +7,18 @@ void myFunction (int myNumbers[5]){
     }
 }
 
+// lets create a function that convert a temperature from Celsius to Fahrenheit
+float celsiusToFahrenheit(float celsius){
+    return (celsius * 9/5) + 32;
+}
+
 int main(){
     int myNumbers[5] = {10, 20, 30, 40, 50};
     // function call
     myFunction(myNumbers);
+    // function call for temperature conversion
+    float celsius = 25.0;
+    float fahrenheit = celsiusToFahrenheit(celsius);
+    printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
     return 0;
 }
